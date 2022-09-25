fd25b1b49eab219067126cbf61193e076e820b4c
Kira Ann <56417097+Astralolgy@users.noreply.github.com>
2022-09-25 11:40:35 +1000

    Add files via upload

--- TheArtExchange/default.js
+++ TheArtExchange/default.js
@@ -1,15 +1,50 @@
-
-
-    let slideIndex = 0;
-    showSlides();
-    function showSlides() {
-        let i;
-        let slides = document.getElementsByClassName("mySlides");
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
+//Creates the about me section, needs buttons and such to compliment it
+//Should be noted that while unusual for an onsite page editor, it's what I managed with my limitations.
+const aboutMe = Vue.createApp({
+    data() {
+        return {
+            Gender: 'Female',
+            Age: 20,
+            Birthday: '01/01/2002',
+            About: 'Say something about yourself'
         }
-        slideIndex++;
-        if (slideIndex > slides.length) {slideIndex = 1}
-        slides[slideIndex-1].style.display = "block";
-        setTimeout(showSlides, 2000); // Change image every 2 seconds
+    },
+    methods: {
+        swapGender() {
+            Gender = 'Female'
+        }
     }
+})
+aboutMe.mount('#aboutMe')
+//Creates the vue object that alternates acc types (needs slight adjustment)
+const profType = Vue.createApp({
+    data() {
+        return {
+            AccType: 'Artist'
+        }
+    },
+    methods: {
+        SwitchAccType() {
+            this.AccType = 'Non-Artist'
+        }
+    }
+})
+profType.mount('#profType')
+
+const contactUs = Vue.createApp({
+    data() {
+        return {
+            name: '',
+            number:'',
+            email:'',
+            inquiry:'',
+            poorInquiry: ''
+        }
+    },
+    methods: {
+        submission() {
+            this.poorInquiry = this.inquiry.length < 150 ? '' : 'Please keep your inquiry clear and to the point.'
+        }
+    }
+})
+contactUs.mount('#contactUs')
